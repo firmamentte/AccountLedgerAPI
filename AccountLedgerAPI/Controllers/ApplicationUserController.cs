b91e166df2adf4b59f6d0c7702c083a4212e9493
Bonginkosi Dlamini <firmament.te@gmail.com>
2022-10-15 18:09:39 +0200

    Register endpoint validation.

--- AccountLedgerAPI/Controllers/ApplicationUserController.cs
+++ AccountLedgerAPI/Controllers/ApplicationUserController.cs
@@ -18,11 +18,91 @@ namespace AccountLedgerAPI.Controllers
 
         [Route("V1/Register")]
         [HttpPost]
-        public async Task<ActionResult> Register([FromBody] RegisterReq  registerReq)
+        public async Task<ActionResult> Register([FromBody] RegisterReq registerReq)
         {
             #region RequestValidation
 
             ModelState.Clear();
+
+            if (string.IsNullOrWhiteSpace(registerReq.FirstName))
+            {
+                ModelState.AddModelError("FirstName", "First Name required");
+            }
+
+            if (string.IsNullOrWhiteSpace(registerReq.LastName))
+            {
+                ModelState.AddModelError("LastName", "Last Name required");
+            }
+
+            if (registerReq.DOB == DateTime.MinValue)
+            {
+                ModelState.AddModelError("InvalidDOB", "Invalid DOB");
+            }
+            else
+            {
+                if (registerReq.DOB > DateTime.Now)
+                {
+                    ModelState.AddModelError("InvalidDOB", "DOB can not be greater than current date");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(registerReq.Contact.MobileNumber))
+            {
+                ModelState.AddModelError("MobileNumber", "Mobile Number required");
+            }
+
+            if (string.IsNullOrWhiteSpace(registerReq.Contact.EmailAddress))
+            {
+                ModelState.AddModelError("EmailAddress", "Email Address required");
+            }
+            else
+            {
+                if (!FirmamentUtilities.Utilities.EmailHelper.IsEmailAddress(registerReq.Contact.EmailAddress))
+                {
+                    ModelState.AddModelError("InvalidEmailAddress", "Invalid Email Address");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(registerReq.Contact.AddressLine1))
+            {
+                ModelState.AddModelError("AddressLine1", "Address Line 1 required");
+            }
+
+            if (string.IsNullOrWhiteSpace(registerReq.Contact.StateOrProvince))
+            {
+                ModelState.AddModelError("StateOrProvince", "State or Province required");
+            }
+
+            if (string.IsNullOrWhiteSpace(registerReq.Contact.CityOrTown))
+            {
+                ModelState.AddModelError("CityOrTown", "City or Town required");
+            }
+
+            if (string.IsNullOrWhiteSpace(registerReq.Contact.SuburbOrTownship))
+            {
+                ModelState.AddModelError("SuburbOrTownship", "Suburb or Township required");
+            }
+
+            if (string.IsNullOrWhiteSpace(registerReq.Contact.PostalCode))
+            {
+                ModelState.AddModelError("PostalCode", "Postal Code required");
+            }
+            else
+            {
+                try
+                {
+                    int.Parse(registerReq.Contact.PostalCode);
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("InvalidPostalCode", "Postal Code must be an integer value");
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ApiErrorResp(ModelState));
+            }
 
             #endregion
 
